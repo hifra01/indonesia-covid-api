5b4a887ea6e632999454cf91f9fb6b3c013e7bb7
Hilman F. Rakhmad <hifraofficial@gmail.com>
2022-01-02 21:18:58 +0700

    refactor yearly services to its own file

--- app/routers/yearly.py
+++ app/routers/yearly.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from typing import Optional
-from app.services.case_data import fetch_yearly, fetch_yearly_on_year
+from app.services.yearly_services import fetch_yearly, fetch_yearly_on_year
 from app.validator.query_validator import validate_yearly_format
 from app.validator.path_validator import validate_year_format
 
@@ -12,11 +12,11 @@ async def get_yearly_data(since: Optional[str] = None, upto: Optional[str] = Non
     if since != None: validate_yearly_format(since)
     if upto != None: validate_yearly_format(upto)
 
-    case_data = await fetch_yearly(since, upto)
+    data = await fetch_yearly(since, upto)
     return {
         "ok": True,
         "message": "Data fetched successfully",
-        "data": case_data
+        "data": data
     }
 
 @router.get("/yearly/{year}", tags=["yearly"])
---
+++ app/services/yearly_services.py
@@ -0,0 +1,108 @@
+from fastapi import HTTPException
+from app.services.data_fetcher import daily_cases, daily_vaccines
+from app.utils import key_parser
+from app.utils.data_mapper import sum_case_to_dict, sum_vaccine_to_dict
+
+async def fetch_yearly(since, upto):
+    case_data = await daily_cases()
+    vaccine_data = await daily_vaccines()
+
+    if since is not None:
+        since_int = int(since)
+        case_data = list(
+            filter(
+                lambda data: key_parser.parse_to_year_int(data["key"]) >= since_int,
+                case_data 
+            )
+        )
+
+        vaccine_data = list(
+            filter(
+                lambda data: key_parser.parse_to_year_int(data["key"]) >= since_int,
+                vaccine_data 
+            )
+        )
+
+    if upto is not None:
+        upto_int = int(since)
+        case_data = list(
+            filter(
+                lambda data: key_parser.parse_to_year_int(data["key"]) <= upto_int,
+                case_data 
+            )
+        )
+
+        vaccine_data = list(
+            filter(
+                lambda data: key_parser.parse_to_year_int(data["key"]) <= upto_int,
+                vaccine_data 
+            )
+        )
+
+    yearly_data = []
+
+    years = sorted(set(map(lambda data: key_parser.parse_to_year_int(data["key"]), case_data)))
+
+    for year in years:
+        cases_on_year = list(
+            filter(
+                lambda data: key_parser.parse_to_year_int(data["key"]) == year,
+                case_data
+            )
+        )
+        mapped_case = sum_case_to_dict(cases_on_year)
+        mapped_vaccine = dict()
+
+        vaccine_on_year = list(
+            filter(
+                lambda data: key_parser.parse_to_year_int(data["key"]) == year,
+                vaccine_data
+            )
+        )
+
+        if len(vaccine_on_year) > 0:
+            mapped_vaccine = sum_vaccine_to_dict(vaccine_on_year)
+
+        yearly_data.append({
+            "year": str(year),
+            **mapped_case,
+            **mapped_vaccine
+        })
+    
+    return yearly_data
+
+async def fetch_yearly_on_year(year):
+    case_data = await daily_cases()
+    vaccine_data = await daily_vaccines()
+
+    year_int = int(year)
+    
+    cases_on_year = list(
+        filter(
+            lambda data: key_parser.parse_to_year_int(data["key"]) == year_int,
+            case_data
+        )
+    )
+
+    if len(cases_on_year) == 0:
+        raise HTTPException(status_code=404, detail="Data not found")
+
+    vaccine_on_year = list(
+        filter(
+            lambda data: key_parser.parse_to_year_int(data["key"]) == year,
+            vaccine_data
+        )
+    )
+
+    mapped_case = sum_case_to_dict(cases_on_year)
+    mapped_vaccine = dict()
+
+    if len(vaccine_on_year) > 0:
+        mapped_vaccine = sum_vaccine_to_dict(vaccine_on_year)
+
+    return {
+        "year": year,
+        **mapped_case,
+        **mapped_vaccine
+    }
+    
