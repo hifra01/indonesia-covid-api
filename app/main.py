05ef0a06c698a0c4bdf1a07895cd5f446b1978e4
Hilman F. Rakhmad <hifraofficial@gmail.com>
2022-01-02 22:03:48 +0700

    implement exception handlers

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from app.routers import root, yearly, monthly, daily
 
 app = FastAPI()
@@ -7,3 +9,23 @@ app.include_router(root.router)
 app.include_router(yearly.router)
 app.include_router(monthly.router)
 app.include_router(daily.router)
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(
+        content={
+            "ok": False,
+            "message": exc.detail
+        },
+        status_code=exc.status_code
+    )
+
+@app.exception_handler(Exception)
+async def server_exception_handler(request, exc):
+    return JSONResponse(
+        content={
+            "ok": False,
+            "message": "Internal Server Error"
+        },
+        status_code=500
+    )
