419ef0007c83a9f1551a8da993f7ecde95cfcf27
Hilman F. Rakhmad <hifraofficial@gmail.com>
2022-01-02 12:47:05 +0700

    add valid year check on query validator

--- app/validator/query_validator.py
+++ app/validator/query_validator.py
@@ -1,18 +1,28 @@
 from fastapi import HTTPException
 import re
-from datetime import datetime
+from datetime import datetime, timezone
 
 def validate_yearly_format(year_str: str):
     if re.match(r"^\d{4}$", year_str) == None:
         raise HTTPException(status_code=400, detail="Wrong year format on query. Correct format: YYYY (e.g. 2021)")
+
+    year = int(year_str)
+
+    if year < 1970:
+        raise HTTPException(status_code=400, detail="Year can only start from 1970")
 
 def validate_monthly_format(month_str: str):
     if re.match(r"^\d{4}\.[0-1]\d$", month_str) == None:
         raise HTTPException(status_code=400, detail="Wrong month format on query. Correct format: YYYY.MM (e.g. 2021.05)")
     
     month_splitted = month_str.split(".")
+
+    year = int(month_splitted[0])
     month = int(month_splitted[1])
-    
+
+    if year < 1970:
+        raise HTTPException(status_code=400, detail="Year can only start from 1970")
+
     if month < 1 or month > 12:
         raise HTTPException(status_code=400, detail="Month can only be between 01 and 12")
 
@@ -25,10 +35,13 @@ def validate_daily_format(date_str: str):
     month = int(date_splitted[1])
     date = int(date_splitted[2])
 
+    if year < 1970:
+        raise HTTPException(status_code=400, detail="Year can only start from 1970")
+
     if month < 1 or month > 12:
         raise HTTPException(status_code=400, detail="Month can only be between 01 and 12")
 
     try:
-        newDate = datetime(year, month, date)
+        newDate = datetime(year, month, date, tzinfo=timezone.utc)
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid date")
